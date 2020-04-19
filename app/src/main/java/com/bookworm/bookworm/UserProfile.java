4e1c6e2f5888b35fe8e57788de12198724462274
manishdadwal85 <manishdadwal1985@gmail.com>
2020-04-20 01:03:31 +0530

    Profile 2nd commit

--- app/src/main/java/com/bookworm/bookworm/UserProfile.java
+++ app/src/main/java/com/bookworm/bookworm/UserProfile.java
@@ -3,6 +3,10 @@ package com.bookworm.bookworm;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.bookworm.bookworm.Model.User;
@@ -16,8 +20,14 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
     String email;
-    TextView emailtxt;
+
     TextView userName;
+    TextView emailtxt;
+    EditText phone;
+    EditText pincode;
+    ImageButton editpin;
+    ImageButton  editphone;
+
     FirebaseUser firebaseUser;
     DatabaseReference reference;
 
@@ -27,9 +37,14 @@ public class UserProfile extends AppCompatActivity {
         setContentView(R.layout.activity_userprofile);
 
         userName = findViewById(R.id.username);
+        emailtxt = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        pincode = findViewById(R.id.pincode);
+        editphone = findViewById(R.id.editPhone);
+        editpin = findViewById(R.id.editPin);
+        editpin.setTag(R.drawable.ic_edit_24px);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String val = firebaseUser.getUid();
         email = firebaseUser.getEmail().replaceAll("[.]", "_");
         reference = FirebaseDatabase.getInstance().getReference("users").child(email).child("details");
         reference.addValueEventListener(new ValueEventListener() {
@@ -37,6 +52,9 @@ public class UserProfile extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
                 userName.setText(user.getFullname());
+                emailtxt.setText(user.getEmail());
+                phone.setText(user.getPhone());
+                pincode.setText(user.getPincode());
             }
 
             @Override
@@ -44,5 +62,35 @@ public class UserProfile extends AppCompatActivity {
 
             }
         });
+
+        editpin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer resource = (Integer) editpin.getTag();
+
+                if (resource == R.drawable.ic_edit_24px){
+                    pincode.setEnabled(true);
+                    editpin.setImageResource(R.drawable.ic_done_24px);
+                    editpin.setTag(R.drawable.ic_done_24px);
+                }
+                else{
+                    pincode.setEnabled(false);
+                    editpin.setImageResource(R.drawable.ic_edit_24px);
+                    editpin.setTag(R.drawable.ic_edit_24px);
+                }
+
+//                editpin.setBackgroundResource(R.drawable.ic_done_24px);
+            }
+        });
+
+        editphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phone.setEnabled(true);
+                editphone.setImageResource(R.drawable.ic_done_24px);
+//                editphone.setBackgroundResource(R.drawable.ic_done_24px);
+            }
+        });
+
     }
 }
